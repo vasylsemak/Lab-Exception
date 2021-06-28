1bd387ff994b879249cd6bd8a0ebb2416229157d
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-27 20:10:34 -0400

    modify: getInput method - throw custom EvenNumberException if num is even

--- src/com/vasyl/exceptions/EvenNumberException.java
+++ src/com/vasyl/exceptions/EvenNumberException.java
@@ -1,5 +1,9 @@
 package com.vasyl.exceptions;
 
-public class EvenNumberException {
-
+@SuppressWarnings("serial")
+public class EvenNumberException extends Exception {
+//	@Override
+	public String getMessage() {
+		return "You cannot input an even number.";
+	}
 }
--- src/com/vasyl/main/Simulation.java
+++ src/com/vasyl/main/Simulation.java
@@ -1,19 +1,32 @@
 package com.vasyl.main;
 
 import java.util.Scanner;
+import com.vasyl.exceptions.EvenNumberException;
 
 public class Simulation {
 
 	public static void main(String[] args) {
-		getInput();
+		try {
+			getInput();
+		} catch(EvenNumberException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public static void getInput() {
+	public static void getInput() throws EvenNumberException {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Please, enter your number here and press Enter...");
-		String inputNum = in.nextLine();
-		System.out.println("Your number is: " + inputNum);
+		String stringNum = in.nextLine();
+		System.out.println("Your number is: " + stringNum);
+		
+		if(isEven(stringNum)) {
+			try {
+				throw new EvenNumberException();
+			} finally {
+				in.close();
+			}
+		}
 		
 		in.close();
 	}
@@ -22,5 +35,5 @@ public class Simulation {
 		int i = Integer.parseInt(num); 
 		return (i % 2 == 0);
 	}
-
+	
 }
